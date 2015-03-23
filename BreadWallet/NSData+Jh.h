17c2f969b99901f58bb8173f4da3dded23182d3e
“Quantum Explorer” <quantumexplorer@dashpay.io>
2015-03-24 00:49:20 +0800

    added groestl and jh

---
+++ BreadWallet/NSData+Jh.h
@@ -0,0 +1,13 @@
+//
+//  NSData+Jh.h
+//  BreadWallet
+//
+//  Created by Quantum Explorer on 3/22/15.
+//  Copyright (c) 2015 Aaron Voisine. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData(Jh)
+
+@end
