324b4f419d368c43f0d02f385e4eb306176dd8a2
Quantum Explorer <quantumexplorer@dashpay.io>
2015-09-17 02:05:51 +0800

    cleaned project

--- BreadWallet/DCMasternodeControlTableViewController.h
+++ BreadWallet/DCMasternodeControlTableViewController.h
@@ -2,7 +2,7 @@
 //  DCMasternodeControlTableViewController.h
 //  DashWallet
 //
-//  Created by  Quantum Exploreron 9/1/15.
+//  Created by Quantum Explorer on 9/1/15.
 //  Copyright (c) 2015 Aaron Voisine. All rights reserved.
 //
 
