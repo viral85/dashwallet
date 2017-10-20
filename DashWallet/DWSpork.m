83761a296be2448e55adb5666496f9fb4784e641
Quantum Explorer <quantumexplorer@dashpay.io>
2017-10-20 15:02:11 +0700

    sporks working

--- DashWallet/DWSpork.m
+++ DashWallet/DWSpork.m
@@ -9,10 +9,17 @@
 #import "DWSpork.h"
 #import "NSData+Dash.h"
 #import "NSData+Bitcoin.h"
+#import "NSString+Dash.h"
+#import "NSString+Bitcoin.h"
+#import "NSMutableData+Bitcoin.h"
 #import "BRKey.h"
 
+#if !DASH_TESTNET
 #define SPORK_PUBLIC_KEY @"04549ac134f694c0243f503e8c8a9a986f5de6610049c40b07816809b0d1d06a21b07be27b9bb555931773f62ba6cf35a25fd52f694d4e1106ccd237a7bb899fdd"
-#define SPORK_MESSAGE_MAGIC @"Darkcoin Signed Message"
+#else
+#define SPORK_PUBLIC_KEY @"046f78dcf911fbd61910136f7f0f8d90578f68d0b3ac973b5040fb7afb501b5939f39b108b0569dca71488f5bbf498d92e4d1194f6f941307ffd95f75e76869f0e"
+#endif
+#define SPORK_MESSAGE_MAGIC @"DarkCoin Signed Message:\n"
 
 @implementation DWSpork
 
@@ -37,10 +44,14 @@
 }
 
 -(BOOL)checkSignature:(NSData*)signature {
-    NSString * stringMessage = [NSString stringWithFormat:@"%@%d%llu%llu",SPORK_MESSAGE_MAGIC,self.identifier,self.value,self.timeSigned];
+    NSString * stringMessage = [NSString stringWithFormat:@"%d%llu%llu",self.identifier,self.value,self.timeSigned];
+    NSMutableData * stringMessageData = [NSMutableData data];
+    [stringMessageData appendString:SPORK_MESSAGE_MAGIC];
+    [stringMessageData appendString:stringMessage];
     BRKey * sporkPublicKey = [BRKey keyWithPublicKey:[NSData dataFromHexString:SPORK_PUBLIC_KEY]];
-    UInt256 messageDigest = [stringMessage dataUsingEncoding:NSUTF8StringEncoding].SHA256;
-    return [sporkPublicKey verify:messageDigest signature:signature];
+    UInt256 messageDigest = stringMessageData.SHA256_2;
+    BRKey * messagePublicKey = [BRKey keyRecoveredFromCompactSig:signature andMessageDigest:messageDigest];
+    return [sporkPublicKey.publicKey isEqualToData:messagePublicKey.publicKey];
 }
 
 @end
